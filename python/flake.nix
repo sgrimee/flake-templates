0b751dfeee7d6cfddf1646572c8325593f2897de
sgrimee <sgrimee@gmail.com.me>
2024-08-12 15:53:01 +0200

    use uv instead of pip, flake

--- python/flake.nix
+++ python/flake.nix
@@ -3,23 +3,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
     flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
         with pkgs; {
           devShells.default = mkShell {
-            #nativeBuildInputs = [pkg-config];
-            buildInputs = [python312];
+            # nativeBuildInputs = [ ];
+            buildInputs = [ python312 uv ];
           };
         }
-    );
+      );
 }
