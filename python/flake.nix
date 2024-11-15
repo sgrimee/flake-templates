57bc529cb1f4ea0f49c576d48cec4e2473bf6cd3
sgrimee <sgrimee@gmail.com.me>
2024-11-15 15:27:47 +0100

    update python

--- python/flake.nix
+++ python/flake.nix
@@ -1,27 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    ,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
         with pkgs; {
           devShells.default = mkShell {
-            # nativeBuildInputs = [ ];
-            buildInputs = [ python312 uv ];
+            buildInputs = [
+              git-cliff
+              (python312.withPackages(ps: with ps; [
+                # Add your python dependencies here
+              ]))
+              ruff
+              uv
+            ];
           };
         }
-      );
+    );
 }
